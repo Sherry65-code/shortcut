c2642fd49a8d8a4626ffc52aeb8e00233981e4ec
Sherry65-code <sparambir65@gmail.com>
2022-10-11 20:37:29 +0530

    Anime1

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ window.addEventListener('scroll',function(){
     if (scrollHeight > 400){
         scrollHeight = 400;
     }
-    
+    if (scrollHeight >= 260){
+        document.getElementById('header').style.backgroundColor  ="white";
+    }
+    else if (scrollHeight < 260){
+        document.getElementById('header').style.backgroundColor  ="#00000000";
+    }
     document.getElementById('swipe').style.opacity = 1-(scrollHeight/400);
 });

a080c4ad785df6852d640b3fb746af82026e6f2a
Sherry65-code <sparambir65@gmail.com>
2022-10-11 21:31:56 +0530

    parallax effect

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ window.addEventListener('scroll',function(){
     else if (scrollHeight < 260){
         document.getElementById('header').style.backgroundColor  ="#00000000";
     }
+    this.document.getElementById('c1').style.left = 1-(scrollHeight/400)*100 + 'px';
+    this.document.getElementById('c2').style.right = 1-(scrollHeight/400)*100 + 'px';
     document.getElementById('swipe').style.opacity = 1-(scrollHeight/400);
 });
